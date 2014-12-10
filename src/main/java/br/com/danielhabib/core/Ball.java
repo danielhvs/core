a5275a0b4a16fe288a87d2b61699de8e49462fe4
danielhvs <danielhvs70@gmail.com>
2014-12-10 12:58:57 -0200

    Adjusting ball size and number position

--- src/main/java/br/com/danielhabib/core/Ball.java
+++ src/main/java/br/com/danielhabib/core/Ball.java
@@ -5,6 +5,9 @@ import java.awt.Graphics;
 
 public class Ball extends PsicoComponent {
 
+	private static final int NUMBER_X_OFFSET = -3;
+	private static final int NUMBER_Y_OFFSET = 5;
+
 	public Ball(Position position) {
 		super(position);
 	}
@@ -14,11 +17,11 @@ public class Ball extends PsicoComponent {
 		g.setColor(Color.BLUE);
 		int x = getPosition().getX();
 		int y = getPosition().getY();
-		int offset = 3 * Config.SIZE / 8;
-		int ballSize = Config.SIZE / 4;
+		int offset = Config.SIZE / 4;
+		int ballSize = Config.SIZE / 2;
 		g.fillOval(x + offset, y + offset, ballSize, ballSize);
 		g.setColor(Color.YELLOW);
-		g.drawString("1", x + offset + ballSize / 3, y + offset + 9 * ballSize / 10);
+		g.drawString("1", x + offset + NUMBER_X_OFFSET + ballSize / 2, y + offset + NUMBER_Y_OFFSET + ballSize / 2);
 	}
 
 }
