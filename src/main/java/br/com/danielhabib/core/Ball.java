acc51257660e2faacedc3af426f57f2486ce628c
danielhvs <danielhvs70@gmail.com>
2014-12-13 19:12:14 -0200

    Fancy ball drawing

--- src/main/java/br/com/danielhabib/core/Ball.java
+++ src/main/java/br/com/danielhabib/core/Ball.java
@@ -5,6 +5,9 @@ import java.awt.Graphics;
 
 public class Ball extends PsicoComponent {
 
+	// Must be even
+	private static final int INSIDE_DIAMETER_OFFSET = 8;
+	private static final int INSIDE_OFFSET = INSIDE_DIAMETER_OFFSET / 2;
 	public static int OFFSET = Config.SIZE / 4;
 	public static int DIAMETER = Config.SIZE / 2;
 
@@ -14,10 +17,13 @@ public class Ball extends PsicoComponent {
 
 	@Override
 	public void draw(Graphics g) {
-		g.setColor(Color.BLUE);
 		int x = getPosition().getX();
 		int y = getPosition().getY();
+		g.setColor(Color.RED);
 		g.fillOval(x + OFFSET, y + OFFSET, DIAMETER, DIAMETER);
+		g.setColor(Color.BLUE);
+		g.fillOval(x + OFFSET + INSIDE_OFFSET, y + OFFSET + INSIDE_OFFSET, DIAMETER - INSIDE_DIAMETER_OFFSET, DIAMETER
+				- INSIDE_DIAMETER_OFFSET);
 	}
 
 }
